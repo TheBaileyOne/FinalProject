c2024de159a941f63079ce5caad0f2759409cc85
Alex <38739193+TheBaileyOne@users.noreply.github.com>
2019-12-30 20:46:48 +0000

    initial commit

---
+++ app/src/main/java/com/example/studentlifeapp/activities/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.studentlifeapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.IdRes
+import androidx.viewpager.widget.ViewPager
+import com.example.studentlifeapp.R
+import com.example.studentlifeapp.pagers.MainPagerAdapter
+import com.example.studentlifeapp.pagers.MainScreen
+import com.example.studentlifeapp.pagers.getMainScreenForMenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var viewPager: ViewPager
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var mainPagerAdapter:MainPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //initialize views
+        viewPager=findViewById(R.id.view_pager)
+        bottomNavigationView=findViewById(R.id.bottom_navigation_view)
+        mainPagerAdapter = MainPagerAdapter(supportFragmentManager)
+
+        //set items to be displayed
+        mainPagerAdapter.setItems(arrayListOf(MainScreen.DASHBOARD, MainScreen.TIMETABLE, MainScreen.SUBJECTS, MainScreen.STUDYMODE))
+
+        //show default screen
+        val defaultScreen = MainScreen.DASHBOARD
+        scrollToScreen(defaultScreen)
+        selectBottomNavigationViewMenuItem(defaultScreen.menuItemId)
+        supportActionBar?.setTitle(defaultScreen.titleStringId)
+
+        //Set the listener for item selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        //Attach adapter to view pager to make it select bottom navigation
+        //change to right values when selected
+        viewPager.adapter = mainPagerAdapter
+        viewPager.addOnPageChangeListener(object:ViewPager.SimpleOnPageChangeListener(){
+            override fun onPageSelected(position: Int) {
+                    val selectedScreen = mainPagerAdapter.getItems() [position]
+                    selectBottomNavigationViewMenuItem(selectedScreen.menuItemId)
+                    supportActionBar?.setTitle(selectedScreen.titleStringId)
+
+            }
+        })
+    }
+
+    //scrolls ViewPager to show the screen
+    private fun scrollToScreen(mainScreen:MainScreen){
+        val screenPosition = mainPagerAdapter.getItems().indexOf(mainScreen)
+        if(screenPosition != viewPager.currentItem){
+            viewPager.currentItem = screenPosition
+        }
+    }
+
+    //select item in bottom navigation
+    private fun selectBottomNavigationViewMenuItem(@IdRes menuItemId: Int){
+        bottomNavigationView.setOnNavigationItemSelectedListener(null)
+        bottomNavigationView.selectedItemId=menuItemId
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    //listener for registering navigation clicks
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        getMainScreenForMenuItem(menuItem.itemId)?.let{
+            scrollToScreen(it)
+            supportActionBar?.setTitle(it.titleStringId)
+            return true
+        }
+        return false
+    }
+
+}
---
+++ app/src/main/java/com/example/studentlifeapp/pagers/MainScreen.kt
@@ -0,0 +1,33 @@
+package com.example.studentlifeapp.pagers
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.IdRes
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import com.example.studentlifeapp.R
+import com.example.studentlifeapp.fragments.*
+
+// screens available for display in the main screen
+enum class MainScreen(@IdRes val menuItemId: Int,
+                      @DrawableRes val menuItemIconId: Int,
+                      @StringRes val titleStringId: Int,
+                      val fragment: Fragment
+){
+    DASHBOARD(R.id.bottom_navigation_item_dashboard, R.drawable.ic_dashboard,
+        R.string.activity_main_bottom_screen_dashboard, DashboardFragment()),
+    TIMETABLE(R.id.bottom_navigation_item_timeTable,R.drawable.ic_date_range,
+        R.string.activity_main_bottom_screen_timeTable, TimetableFragment()),
+    SUBJECTS(R.id.bottom_navigation_item_subjects,R.drawable.ic_school,
+        R.string.activity_main_bottom_screen_subjects, SubjectFragment()),
+    STUDYMODE(R.id.bottom_navigation_item_studyMode,R.drawable.ic_local_library,
+        R.string.activity_main_bottom_screen_studyMode,StudyModeFragment())
+}
+
+fun getMainScreenForMenuItem(menuItemId: Int): MainScreen? {
+    for (mainScreen in MainScreen.values()) {
+        if (mainScreen.menuItemId == menuItemId){
+            return mainScreen
+        }
+    }
+    return null
+}
