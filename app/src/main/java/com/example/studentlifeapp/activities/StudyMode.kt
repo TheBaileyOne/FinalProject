5cc2c49d151a16cd18549b36e24158c6703745e2
Alex <38739193+TheBaileyOne@users.noreply.github.com>
2019-12-31 10:21:29 +0000

    StudyModeFragment opens StudyMode activity with basic layout

---
+++ app/src/main/java/com/example/studentlifeapp/activities/StudyMode.kt
@@ -0,0 +1,26 @@
+package com.example.studentlifeapp.activities
+
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.studentlifeapp.R
+
+import kotlinx.android.synthetic.main.activity_study_mode.*
+
+class StudyMode : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_study_mode)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = "     Timer"
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //Ads items to the action bar if preseent
+        menuInflater.inflate(R.menu.menu_study_mode, menu)
+        return true
+    }
+
+}
--- app/src/main/java/com/example/studentlifeapp/fragments/StudyModeFragment.kt
+++ app/src/main/java/com/example/studentlifeapp/fragments/StudyModeFragment.kt
@@ -1,14 +1,18 @@
 package com.example.studentlifeapp.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.studentlifeapp.R
+import com.example.studentlifeapp.activities.StudyMode
+import kotlinx.android.synthetic.main.fragment_study_mode.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,7 +37,17 @@ class StudyModeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_study_mode, container, false)
+        //return inflater.inflate(R.layout.fragment_study_mode, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_study_mode, container, false)
+
+        view.studyButton.setOnClickListener{ view ->
+            Log.d("studyButton","Selected")
+            val intent = Intent(context, StudyMode::class.java);
+            startActivity(intent);
+
+        }
+        return view
+
     }
 
 
