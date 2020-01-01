31f71bc7518d38c60cfb6e15627941fe907e6bbc
Alex <38739193+TheBaileyOne@users.noreply.github.com>
2020-01-01 18:34:01 +0000

    update TODOs

--- app/src/main/java/com/example/studentlifeapp/fragments/StudyModeFragment.kt
+++ app/src/main/java/com/example/studentlifeapp/fragments/StudyModeFragment.kt
@@ -14,23 +14,9 @@ import com.example.studentlifeapp.R
 import com.example.studentlifeapp.activities.StudyMode
 import kotlinx.android.synthetic.main.fragment_study_mode.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [StudyModeFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [StudyModeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudyModeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/studentlifeapp/fragments/SubjectFragment.kt
+++ app/src/main/java/com/example/studentlifeapp/fragments/SubjectFragment.kt
@@ -10,23 +10,8 @@ import android.view.ViewGroup
 
 import com.example.studentlifeapp.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SubjectFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SubjectFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SubjectFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
 
     override fun onCreateView(
