ad2a1f679b42557eccd9715a745ba2f582b510f3
Alex <38739193+TheBaileyOne@users.noreply.github.com>
2020-04-22 09:56:48 +0100

    fix StudyGenerator

--- app/src/main/java/com/example/studentlifeapp/util/StudyGenerator.kt
+++ app/src/main/java/com/example/studentlifeapp/util/StudyGenerator.kt
@@ -40,78 +40,82 @@ class StudyGenerator(private val name:String, private val startDate: LocalDateTi
         while (day<=endDate){
             var dayTime = dailyStudy //amount of time spent studying on that day
             var time = timeBeg
-//            var freeTime = false
             if(!weekendStudy && day.dayOfWeek == DayOfWeek.SATURDAY ){
                 day = day.plusDays(2)
+                if (day>endDate) break
             }
             else if (!weekendStudy && day.dayOfWeek == DayOfWeek.SUNDAY ){
                 day = day.plusDays(1)
+                if (day>endDate)break
             }
-            else {
-                if (checkDayEvents(day)) {
-                    val todayEvents =
-                        (groupedEvents[day] ?: error("Should not be null")).toMutableList()
-                    do {
-                        Log.d("Today Events", "$day: ${todayEvents.size}")
-                        val firstOverlap = checkOverlapEvents(todayEvents, time)
-                        when {
-                            firstOverlap != null -> {
-                                time = firstOverlap.endTime.toLocalTime()
-                                todayEvents.remove(firstOverlap)
-                            }
-                            checkOverlap(time, lunchTime, lunchTime.plusHours(1)) -> {
-                                time = lunchTime.plusHours(1)
-                            }
-                            else -> {
-                                val eventLength = if (dayTime == 1L || checkOverlapEvents(
-                                        todayEvents,
-                                        time.plusHours(1)
-                                    ) != null
-                                ) 1L
-                                else if (!weekendStudy && checkOverlapEvents(
-                                        todayEvents,
-                                        time.plusHours(3)
-                                    ) == null
-                                ) 3L
-                                else 2L
-                                studyList.add(
-                                    Event(
-                                        "$name: Study",
-                                        EventType.STUDY,
-                                        LocalDateTime.of(day, time),
-                                        LocalDateTime.of(day, time.plusHours(eventLength))
-                                    )
-                                )
-                                dayTime -= eventLength
-                            }
+            if (checkDayEvents(day)) {
+                val todayEvents =
+                    (groupedEvents[day] ?: error("Should not be null")).toMutableList()
+                do {
+                    Log.d("Today Events", "$day: ${todayEvents.size}")
+                    val firstOverlap = checkOverlapEvents(todayEvents, time)
+                    when {
+                        firstOverlap != null -> {
+                            time = firstOverlap.endTime.toLocalTime()
+                            todayEvents.remove(firstOverlap)
                         }
-                    } while ((time.plusHours(1).isBefore(timeEnd) && dayTime > 0))
-                    //                }while ((time.plusHours(1).isBefore(timeEnd) && !freeTime))
-                } else {
-                    studyList.add(
-                        Event(
-                            "$name: Study",
-                            EventType.STUDY,
-                            LocalDateTime.of(day, timeBeg),
-                            LocalDateTime.of(day, timeBeg.plusHours(dailyStudy))
-                        )
+                        checkOverlap(time, lunchTime, lunchTime.plusHours(1)) -> {
+                            time = lunchTime.plusHours(1)
+                        }
+                        else -> {
+                            val eventLength = if (dayTime == 1L || checkOverlapEvents(
+                                    todayEvents,
+                                    time.plusHours(1)
+                                ) != null || checkOverlap(time.plusHours(1),lunchTime,lunchTime.plusHours(1))
+                            ) 1L //If there is an event/lunch after 1 hour have length of study be 1
+                            else if (!weekendStudy && checkOverlapEvents(
+                                    todayEvents,
+                                    time.plusHours(3)
+                                ) == null || !checkOverlap(time.plusHours(2),lunchTime,lunchTime.plusHours(1))
+                            ) 3L //If weekend study and no events/lunch in 3 hours have study length be 3
+                            else 2L
+                            studyList.add(
+                                Event(
+                                    "$name: Study",
+                                    EventType.STUDY,
+                                    LocalDateTime.of(day, time),
+                                    LocalDateTime.of(day, time.plusHours(eventLength))
+                                )
+                            )
+                            time = time.plusHours(eventLength)
+                            dayTime -= eventLength
+                        }
+                    }
+                } while ((time.plusHours(1).isBefore(timeEnd) && dayTime > 0))
+                //                }while ((time.plusHours(1).isBefore(timeEnd) && !freeTime))
+            } else {
+                studyList.add(
+                    Event(
+                        "$name: Study",
+                        EventType.STUDY,
+                        LocalDateTime.of(day, timeBeg),
+                        LocalDateTime.of(day, timeBeg.plusHours(dailyStudy))
                     )
-                }
-                day = day.plusDays(dayInterval)
+                )
             }
+            day = day.plusDays(dayInterval)
+
 
         }
         Log.d("Study Generator", "generated study events: $studyList")
     }
 
+    //function to check if events occur on that day
+    private fun checkDayEvents(day: LocalDate)= groupedEvents.containsKey(day)
+
+    //function for finding events that overlap
+    private fun checkOverlapEvents(events: MutableList<Event>, checkTime:LocalTime) = events.find{checkOverlap(checkTime,it.startTime.toLocalTime(),it.endTime.toLocalTime())}
+
+    //function that returns true if there is an overlap in time
     private fun checkOverlap(checkTime:LocalTime, startTime:LocalTime, endTime:LocalTime):Boolean {
         return (checkTime.isBefore(endTime) && checkTime.isAfter(startTime)) || checkTime == startTime ||
                 (checkTime.plusHours(studyLength).isBefore(endTime) && checkTime.plusHours(studyLength).isAfter(startTime))
     }
 
-    private fun checkOverlapEvents(events: MutableList<Event>, checkTime:LocalTime) = events.find{checkOverlap(checkTime,it.startTime.toLocalTime(),it.endTime.toLocalTime())}
-
-    //function to check if events occur on that day
-    private fun checkDayEvents(day: LocalDate)= groupedEvents.containsKey(day)
 
 }
